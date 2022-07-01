661dc6fcd1a756ff19c5a6cd98f932c7068a52f1
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-07-01 22:51:55 +0200

    Feature: Created constants for value and buttons.

---
+++ app.js
@@ -0,0 +1,13 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+
+/**
+ * We are using '#' because we are fetching and id-->value
+ */
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+console.log(value);
+console.log(btns);
