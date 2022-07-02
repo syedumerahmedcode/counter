05dba31556fed3d3e1fce47c024af231808311d1
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-07-02 19:50:01 +0200

    Feature: Code refactoring.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const ZERO = 0;
 // set initial count
-let count = 0;
+
+let count = ZERO;
 
 // select value and buttons
 
@@ -9,30 +11,36 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+const click = "click";
 // console.log(value);
 // console.log(btns);
 
 // Instead of calling AddEventListener for each button one by one,
 // we will use forEach() method instead.
 btns.forEach(function (btn) {
-  btn.addEventListener("click", function (eventObject) {
+  btn.addEventListener(click, function (eventObject) {
+    const blackColor = "#222";
+    const greenColor = "green";
+    const redColor = "red";
     const styles = eventObject.currentTarget.classList;
+    const decrease = "decrease";
+    const increase = "increase";
     // console.log(styles);
-    if (styles.contains("decrease")) {
+    if (styles.contains(decrease)) {
       count--;
-    } else if (styles.contains("increase")) {
+    } else if (styles.contains(increase)) {
       count++;
     } else {
-      count = 0;
+      count = ZERO;
     }
-    if (count > 0) {
-      value.style.color = "green";
+    if (count > ZERO) {
+      value.style.color = greenColor;
     }
-    if (count < 0) {
-      value.style.color = "red";
+    if (count < ZERO) {
+      value.style.color = redColor;
     }
-    if (count === 0) {
-      value.style.color = "#222";
+    if (count === ZERO) {
+      value.style.color = blackColor;
     }
     value.textContent = count;
   });
